79718e0eea19c3e71de511ebb1ec3947c8c7c5d9
Gianmarco <gianmarco.dato26@gmail.com>
2025-10-30 18:37:37 +0100

    correzione if

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -5,7 +5,7 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
  
 
 for (let i = 1; i < 101; i++) {
-    if (i % 15 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
     console.log('fizzbuzz')
   } else if (i % 5 === 0) {
     console.log('buzz')
